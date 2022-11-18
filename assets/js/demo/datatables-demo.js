72803d9d260635e4385fceec12a91d689f155fe6
LeandroBarbosaBento <67109002+LeandroBarbosaBento@users.noreply.github.com>
2022-11-18 16:54:36 -0300

    add text-center class to the datatable text when there are no clients to be displayed

--- assets/js/demo/datatables-demo.js
+++ assets/js/demo/datatables-demo.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     language: {
       search:         "Pesquisar:",
       lengthMenu:    "Exibir _MENU_ elementos",
-      zeroRecords:    "<h1>Não há dados para serem exibidos</h1>",
+      zeroRecords:    "<h1 class='text-center'>Não há dados para serem exibidos</h1>",
       info:           "Exibindo de _START_ a _END_ de _TOTAL_ elementos",
       paginate: {
           first:      "Primeira",
